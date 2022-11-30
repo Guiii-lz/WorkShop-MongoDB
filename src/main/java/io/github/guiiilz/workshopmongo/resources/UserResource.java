09d6bcdfde21228ecc02eb5f5771e7f86d6e3375
Guilherme Luiz <Guima.luiz@hotmail.com>
2022-11-30 10:29:20 -0300

    Endpoint para retorno de posts por users

--- src/main/java/io/github/guiiilz/workshopmongo/resources/UserResource.java
+++ src/main/java/io/github/guiiilz/workshopmongo/resources/UserResource.java
@@ -9,13 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import io.github.guiiilz.workshopmongo.domain.Post;
 import io.github.guiiilz.workshopmongo.domain.User;
 import io.github.guiiilz.workshopmongo.dto.UserDTO;
 import io.github.guiiilz.workshopmongo.services.UserService;
@@ -64,5 +64,11 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value="/{id}/posts")
+	@GetMapping
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
+	}
 	
 }
