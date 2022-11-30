59f563a40acf07dae97862869f7c529bf649c3e2
Guilherme Luiz <Guima.luiz@hotmail.com>
2022-11-30 17:16:42 -0300

    Consulta com vários critérios envolvendo datas

--- src/main/java/io/github/guiiilz/workshopmongo/resources/PostResource.java
+++ src/main/java/io/github/guiiilz/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package io.github.guiiilz.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,6 +41,16 @@ public class PostResource {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text){
 		text = URL.decodeParam(text);
 		List<Post> list = postService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value="/fullsearch")
+	@GetMapping
+	public ResponseEntity<List<Post>> fullSearch(@RequestParam(value="text", defaultValue = "") String text, @RequestParam(value="minDate", defaultValue = "") String minDate, @RequestParam(value="maxDate", defaultValue = "") String maxDate){
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = postService.fullSearch(text,min,max);
 		return ResponseEntity.ok().body(list);
 	}
 
